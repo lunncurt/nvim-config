112c842419898a5289841233489f53acbc335b5c
lunncurt <lunncurt@msu.edu>
2024-02-27 19:30:04 -0500

    treesitter changes

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -1,12 +1,13 @@
 return {
-	  {"nvim-treesitter/nvim-treesitter",
+	  "nvim-treesitter/nvim-treesitter",
     		build = ":TSUpdate",
     		config = function()
       		local config = require("nvim-treesitter.configs")
       			config.setup({
+				ensure_installed = {"c", "cpp", "lua", "python", "sql", "java", "javascript", "html", "css", "go", "json"},
         			auto_install = true,
         			highlight = { enable = true },
         			indent = { enable = true },
       				})
-    			  end}
-       }
+    			  end
+}
