c0adb42ddfefecc1b848f24e35f0003c003a16a4
lunncurt <lunncurt@msu.edu>
2024-03-04 14:51:54 -0500

    updated neotree config again

--- lua/plugins/neotree.lua
+++ lua/plugins/neotree.lua
@@ -15,7 +15,7 @@ return{
             close_if_last_window = true,
             hijack_netrw_behavior = "open_default",
             window = {
-              position = "right",
+              position = "left",
               width = 30,
             },
             follow_current_file = {
