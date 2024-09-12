5f079b8115d0504dfa7d1a5a12d1c85dd1ab3c30
Curtis Lunn <145430426+lunncurt@users.noreply.github.com>
2024-09-12 02:05:17 -0400

    toggleterm add/mapping

---
+++ lua/plugins/tterm.lua
@@ -0,0 +1,13 @@
+return {
+	{
+		{
+			"akinsho/toggleterm.nvim",
+			config = function()
+				require("toggleterm").setup({
+					direction = "float",
+					vim.keymap.set("n", "<leader>tt", ":ToggleTerm<CR>")
+				})
+			end,
+		},
+	},
+}
